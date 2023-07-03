b56db80b10cfc4006b1380dfe14b9fba6c50a7c3
kylemaps <kyle.mapue@gmail.com>
2023-07-03 22:38:59 +0800

    updated my ansible and terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,32 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
+/* resource "digitalocean_ssh_key" "default" {
+  name       = "My public key"
+  public_key = file("/.ssh/id_rsa.pub")
+} */
+
 resource "digitalocean_droplet" "vps" {
-  name   = "mapz vps"
-  region = "nyc1"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-18-04-x64"
+  name      = "maps-vps"
+  region    = "nyc1"
+  size      = "s-1vcpu-1gb"
+  image     = "ubuntu-18-04-x64"
+  ssh_keys  = [data.digitalocean_ssh_key.terraform.id]
+  #ssh_keys  = [digitalocean_ssh_key.default.fingerprint]
+
+  connection {
+  host        = self.ipv4_address
+  user        = "root"
+  type        = "ssh"
+  private_key = file(var.pvt_key)
+  timeout     = "2m"
+  }
 }
+
+data "digitalocean_ssh_key" "terraform" {
+  name = "mySshKey"
+}
+
 
 output "droplet_ipv4_address" {
   value = digitalocean_droplet.vps.ipv4_address
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,6 @@ variable "digitalocean_token" {
   type        = string
   description = "DigitalOcean API token"
 }
+
+variable "pvt_key" {
+}
